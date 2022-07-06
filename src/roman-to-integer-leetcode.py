9bb63207a075824451279b7f39f58faae1b71bf8
Anthony Leotta <tony@leotta.com>
2022-07-06 02:56:50 -0400

    Create roman-to-integer-leetcode.py

---
+++ src/roman-to-integer-leetcode.py
@@ -0,0 +1,29 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        decoder = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        values = [] # keep track of decoded values
+        length=len(s)
+        letters = list(reversed(s))
+
+        for index2, letter in enumerate(letters):
+            value = decoder[letter]
+            if index2>0:
+                value2 = decoder[letters[index2-1]]
+                if value2 > value:
+                    values.append(-value)
+                else:                    
+                    values.append(value)
+            else:
+                values.append(value)
+
+        total = sum(values)
+        return total
